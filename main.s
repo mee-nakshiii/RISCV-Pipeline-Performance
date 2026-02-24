ea4235daf9fc1b4e7c9a817219152634b893881f
mee-nakshiii <meenakshisudheesh@gmail.com>
2026-02-24 22:13:18 +0530

    Complete Week 1 sorting baseline and hardware configuration

--- main.s
+++ main.s
@@ -1,18 +1,22 @@
-# main.s - Updated Entry Point
-.text              # Tell Ripes this is CODE first
-.globl main
-
-main:
-    la a0, array   # Load Evana's array
-    lw a1, size    # Load size
-    
-    jal ra, insertion_sort  # Run your logic
-    
-    li a7, 10      # Exit
-    ecall
-
-# Move the includes to the BOTTOM
-.include "data.s"
+.include "src/data.s"
 .include "src/utils.s"
 .include "src/insertion.s"
 .include "src/bubbles.s"
+
+.text
+.globl main
+
+main:
+    #INSERTION SORT
+    la a0, array         
+    lw a1, size         
+    jal ra, insertion_sort 
+    
+    #BUBBLE SORT
+    la a0, array         # Reload address
+    lw a1, size          
+    jal ra, bubble_sort 
+    
+    #EXIT PROGRAM
+    li a7, 10         
+    ecall
