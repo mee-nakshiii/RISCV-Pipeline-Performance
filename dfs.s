80cc8cef2477a6d6d250db8637f5a11cf9a0862c
elainemariabinu <elainemariabinu@gmail.com>
2026-03-08 14:25:49 +0530

    Refactor DFS assembly code for clarity and stack usage
    Updated messages and comments for clarity and added stack handling.

--- dfs.s
+++ dfs.s
@@ -1,39 +1,42 @@
 .data
-adj_list:   .word node1, node2, 0    
-node1:      .word 1                     
-node2:      .word 2                    
+node1:      .word 1
+node2:      .word 2
+adj_list:   .word node1, node2, 0
 
-visited:    .zero 64                    # 64-byte visited array 
-size:       .word 16                    # Number of nodes
-msg:        .string "DFS Traversal Completed.\n"
+visited:    .zero 64                  
+size:       .word 16                 
+msg:        .string "DFS Traversal Completed. Check Stack for recursion depth.\n"
 
 .text
 .globl main
 main:
-    li a0, 0             
-    jal ra, dfs          
+    li a0, 0            
+    jal ra, dfs       
 
+    # Print Status to Console
     li a7, 4
     la a0, msg
-    ecall                
+    ecall                #
 
     # Exit Program
     li a7, 10
     ecall
-
 dfs:
+    # Stack Prologue
     addi sp, sp, -16
     sw ra, 12(sp)
-    sw s0, 8(sp)         
+    sw s0, 8(sp)         # Current node
 
-    mv s0, a0           
+    mv s0, a0            # s0 = current node
 
+    # Mark as Visited
     la t0, visited
-    add t1, t0, s0      
+    add t1, t0, s0       # visited[s0]
     li t2, 1
-    sb t2, 0(t1)         
+    sb t2, 0(t1)         # Store Byte: visited[s0] = 1
 
 dfs_done:
+    # Stack Epilogue
     lw ra, 12(sp)
     lw s0, 8(sp)
     addi sp, sp, 16
